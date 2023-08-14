4a73e714d301e7535d01523005e5491998110ed9
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2023-08-14 20:10:32 -0300

    Realice la coneccion a la base de datos

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,25 +1,68 @@
 from sklearn.tree import DecisionTreeClassifier 
+from sklearn.model_selection  import train_test_split
 import pandas as pd
+import mysql.connector
 
-datos1=[25,70,1,'Maíz']
-datos2=[30,80,2,'Trigo']
-datos3=[35,75,3,'Arroz']
-datos4=[25,70,1,'Trigo']
-datos5=[30,80,2,'Arroz']
-datos6=[35,75,3,'Soja']
+
+
+# Configura la información de la base de datos
+db_config = {
+    "host": "localhost",
+    "user": "usu_semillas",
+    "password": "usu_semillas",
+    "database": "tpinvestigacion"
+}
+
+# Conecta a la base de datos
+conn = mysql.connector.connect(**db_config)
+
+# Crea un cursor para ejecutar consultas
+cursor = conn.cursor()
+
+# Ejemplo: Ejecuta una consulta SELECT
+query = "SELECT * FROM semilla"
+cursor.execute(query)
+
+# Obtiene los resultados de la consulta
+results = cursor.fetchall()
+
+# Imprime los resultados
+for row in results:
+    print(row)
+
+# Cierra el cursor y la conexión
+cursor.close()
+conn.close()
+
+
+
+
+datos1=[25,60,1,'Maíz']
+datos2=[30,60,2,'Sorgo']
+datos3=[15,70,3,'Lechuga']
+datos4=[25,80,4,'Arroz']
+datos5=[22.5,60,5,'Tomate']
+datos6=[25,60,6,'Frijoles']
 # Etc.
-data = [datos1, datos2, datos3, datos4, datos5, datos6]  # Añadir más combinaciones si es necesario
+data = [datos1, datos2, datos3, datos4, datos5, datos6]  
+
 
 # Convertir los datos a un DataFrame de pandas
 df = pd.DataFrame(data, columns=['temperatura', 'humedad', 'zona_cultivo', 'semilla_optima'])
+
 
 # Definir las características (X) y las etiquetas (y)
 X = df[['temperatura', 'humedad', 'zona_cultivo']]
 y = df['semilla_optima']
 
+
 # Creamos el modelo de árbol de decisión multietiqueta
 model = DecisionTreeClassifier()
 model_sem = model.fit(X, y)
+
+#Entrenamiento del modelo 
+
+X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.8)
 
 temperatura_actual = float(input("Ingrese la temperatura actual: "))
 humedad_actual = float(input("Ingrese la humedad actual: "))

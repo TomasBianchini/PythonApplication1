b4a28defef5ee5c2c5b0bf9e568b2c874ce51738
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2023-08-15 15:03:21 -0300

    Final

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -43,11 +43,11 @@ intercambiar_columnas(datos, 0 , 3)
 
 
 # Convertir los datos a un DataFrame de pandas
-df = pd.DataFrame(datos, columns=['temperatura', 'humedad', 'tipo_suelo', 'semilla_optima'])
+df = pd.DataFrame(datos, columns=['tipo_suelo', 'temperatura', 'humedad', 'semilla_optima'])
 
 
 # Definir las características (X) y las etiquetas (y)
-X = df[['temperatura', 'humedad', 'tipo_suelo']]
+X = df[['tipo_suelo', 'temperatura', 'humedad']]
 y = df['semilla_optima']
 
 
@@ -55,22 +55,23 @@ y = df['semilla_optima']
 model = DecisionTreeClassifier()
 model_sem = model.fit(X, y)
 
+
 #Entrenamiento del modelo 
 
-#X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.8)
+X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.8)
 
+tipo_suelo_actual = int(input("Ingrese el tipo de suelo del cultivo:\n1-Alfidoles\n2-Aridisoles\n3-Entisoles\n4-Histosoles\n5-Molisoles\n6-Vertisoles\n7-Ultisoles\n8-Inceptisoles\n "))
 temperatura_actual = float(input("Ingrese la temperatura actual: "))
 humedad_actual = float(input("Ingrese la humedad actual: "))
-zona_cultivo_actual = int(input("Ingrese el tipo de suelo del cultivo:\n1-Alfidoles\n2-Aridisoles\n3-Entisoles\n4-Histosoles\n5-Molisoles\n6-Vertisoles\n7-Ultisoles\n8-Inceptisoles\n "))
 
 # Especifica los nombres de columna para las características en el DataFrame de predicciones
-X_pred = pd.DataFrame([[temperatura_actual, humedad_actual, zona_cultivo_actual]], columns=['temperatura', 'humedad', 'tipo_suelo'])
+X_pred = pd.DataFrame([[tipo_suelo_actual, temperatura_actual, humedad_actual]], columns=['tipo_suelo', 'temperatura', 'humedad'])
 
 # Hacer una predicción basada en los valores proporcionados por el usuario
 semilla_optima = model.predict(X_pred)
 
 # Imprimir la recomendación de la semilla óptima
-print("Las semillas óptimas para plantar en función del clima y el tipo de suelo son:")
+print("La semilla óptima para plantar en función del clima y el tipo de suelo es:")
 print(semilla_optima)
 
 

c6e198a2f0ad79e4a287fb617af69c1ebe800826
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2023-09-07 17:06:58 -0300

    Probando con menos elementos

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -55,42 +55,42 @@ tipo_suelo_actual = 1
 temperatura_actual = 32.5
 humedad_actual = 80
 
-for i in range(1, 6):
 
-    # Convertir los datos a un DataFrame de pandas
-    df = pd.DataFrame(datos, columns=['tipo_suelo', 'temperatura', 'humedad', 'semilla_optima'])
+
+datos = datos[-10:]
+
+# Convertir los datos a un DataFrame de pandas
+
+df = pd.DataFrame(datos, columns=['tipo_suelo', 'temperatura', 'humedad', 'semilla_optima'])
 
     # Definir las características (X) y las etiquetas (y)
-    X = df[['tipo_suelo', 'temperatura', 'humedad']]
-    y = df['semilla_optima']
+X = df[['tipo_suelo', 'temperatura', 'humedad']]
+y = df['semilla_optima']
 
     # Creamos el modelo de árbol de decisión multietiqueta
-    model = DecisionTreeClassifier()
-    model_sem = model.fit(X, y)
+model = DecisionTreeClassifier()
+model_sem = model.fit(X, y)
 
     #Entrenamiento del modelo 
 
-    X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.8)
+X_train, X_test, y_tain, y_test = train_test_split(X, y, test_size =0.8)
 
-    # Especifica los nombres de columna para las características en el DataFrame de predicciones
-    X_pred = pd.DataFrame([[tipo_suelo_actual, temperatura_actual, humedad_actual]], columns=['tipo_suelo', 'temperatura', 'humedad'])
+# Especifica los nombres de columna para las características en el DataFrame de predicciones
+X_pred = pd.DataFrame([[tipo_suelo_actual, temperatura_actual, humedad_actual]], columns=['tipo_suelo', 'temperatura', 'humedad'])
 
-    # Hacer una predicción basada en los valores proporcionados por el usuario
-    semilla_optima = model.predict(X_pred)
-    semillas_optimas.append(semilla_optima)
+# Hacer una predicción basada en los valores proporcionados por el usuario
+semilla_optima = model.predict(X_pred)
 
-    datos_filtrados = [fila for fila in datos if fila[3] != semilla_optima]
 
-    datos = datos_filtrados
 
 # Visualizar el árbol de decisión
-plt.figure(figsize=(30, 30))  # Tamaño de la figura
+plt.figure(figsize=(10, 10))  # Tamaño de la figura
 plot_tree(model_sem, feature_names=['tipo_suelo', 'temperatura', 'humedad'], class_names=df['semilla_optima'].unique().tolist(), filled=True, rounded=True)
 plt.show()
 
 
 # Imprimir la recomendación de la semilla óptima
 print("Las semillas recomendadas para plantar \nen función del clima y el tipo de suelo son:")
-for semilla in semillas_optimas:
-    print(semilla[0])
+print(semilla_optima) 
+   
 
